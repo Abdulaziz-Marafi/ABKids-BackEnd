ae833e4756259cea74840982d24b85766f090c31
Abdulaziz-Marafi <azizbm4798@gmail.com>
2025-04-16 12:54:03 +0300

    Redeem Item works properly

---
+++ ABKids-BackEnd/DTOs/ResponseDTOs/RedeemRewardResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ABKids_BackEnd.DTOs.ResponseDTOs
+{
+    public class RedeemRewardResponseDTO
+    {
+        public int RewardId { get; set; }
+        public string RewardName { get; set; }
+        public int PointsSpent { get; set; }
+        public int RemainingPoints { get; set; }
+        public string? Message { get; set; }
+    }
+}
