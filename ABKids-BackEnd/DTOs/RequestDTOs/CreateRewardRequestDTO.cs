904a742dc727286f6772f94697627c07ea2440dc
Abdulaziz-Marafi <azizbm4798@gmail.com>
2025-04-16 11:00:08 +0300

    Create/Get Rewards - Working Neutral Create Reward endpoint -- Uploads reward pic to the appropriate folder - Get reward works and was made to be allow all

--- ABKids-BackEnd/DTOs/RequestDTOs/CreateRewardRequestDTO.cs
+++ ABKids-BackEnd/DTOs/RequestDTOs/CreateRewardRequestDTO.cs
@@ -5,6 +5,6 @@
         public string RewardName { get; set; }
         public string? RewardDescription { get; set; }
         public int RewardPrice { get; set; } // In lotalty points
-        public string? RewardPicture { get; set; }
+        public IFormFile? RewardPicture { get; set; }
     }
 }
